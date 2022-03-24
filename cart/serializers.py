8dddecf559ac708da204d1c99c853b269b47b5df
Sooraj Ep <epsooraj4@gmail.com>
2022-03-24 17:04:00 +0530

    Serializers

--- cart/serializers.py
+++ cart/serializers.py
@@ -1,9 +1,22 @@
 from rest_framework import serializers
 
 from . import models
+from user import serializers as user_serializers
+from product import serializers as product_serializers
+
+
+class ListCartSerializer(serializers.ModelSerializer):
+
+    user = user_serializers.SimpleUserSerializer()
+    product = product_serializers.ProductSerializer()
+    unit = product_serializers.ProductUnitSerializer()
+
+    class Meta:
+        model = models.Cart
+        fields = '__all__'
 
 
 class CartSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Cart
-        fields = '__all__'
+        fields = ('product', 'unit', 'quantity')
--- cart/views.py
+++ cart/views.py
@@ -5,7 +5,13 @@ from . import models
 
 
 class CartViewSet(viewsets.ModelViewSet):
-    serializer_class = serializers.CartSerializer
+    def get_serializer_class(self):
+        if self.action == 'list' or self.action == 'retrieve':
+            return serializers.ListCartSerializer
+        return serializers.CartSerializer
 
     def get_queryset(self):
         return models.Cart.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        return serializer.save(user=self.request.user)
