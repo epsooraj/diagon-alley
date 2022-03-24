4d30ed51b5b4f9556798fda351ec7267d94f38f5
Sooraj Ep <epsooraj4@gmail.com>
2022-03-24 16:46:35 +0530

    Migrations

--- ecomm/urls.py
+++ ecomm/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('api/v1/', include([
         path('product/', include('product.urls')),
         path('category/', include('category.urls')),
+        path('cart/', include('cart.urls')),
     ])),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
