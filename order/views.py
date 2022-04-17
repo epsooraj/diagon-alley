770cf90c8425b9ce86b533c8efc6beef14453849
Sooraj Ep <epsooraj4@gmail.com>
2022-04-18 02:03:58 +0530

    OrderViewSet

--- order/views.py
+++ order/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import viewsets, generics
 
-# Create your views here.
+from . import models as order_models
+from . import serializers as order_serializers
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    # queryset = order_models.Order.objects.filter(user)
+    serializer_class = order_serializers.OrderSerializer\
+
+
+    def get_queryset(self):
+        # if user is admin return all else return corresponding user's order
+        if self.request.user.is_staff:
+            return order_models.Order.objects.all()
+        else:
+            return order_models.Order.objects.filter(user__user_id=self.request.user)
