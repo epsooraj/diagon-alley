ec5d486833dca309a822f87799c671b0daea1d00
Sooraj Ep <epsooraj4@gmail.com>
2022-04-18 17:34:50 +0530

    django-nested-admin

--- product/admin.py
+++ product/admin.py
@@ -1,7 +1,26 @@
 from django.contrib import admin
+import nested_admin
 
 from . import models
 
-admin.site.register(models.Product)
-admin.site.register(models.Unit)
-admin.site.register(models.UnitImage)
+
+class UnitImageInline(nested_admin.NestedStackedInline):
+    model = models.UnitImage
+    extra = 0
+
+
+class UnitAdminInline(nested_admin.NestedTabularInline):
+    inlines = [
+        UnitImageInline,
+    ]
+    model = models.Unit
+    extra = 0
+
+
+class ProductAdmin(nested_admin.NestedModelAdmin):
+    inlines = [
+        UnitAdminInline,
+    ]
+
+
+admin.site.register(models.Product, ProductAdmin)
