04fc85b1a49bb464a9164d09ab0b10d1b5b6d54e
Sooraj Ep <epsooraj4@gmail.com>
2022-04-18 17:34:24 +0530

    django-nested-admin

--- ecomm/settings.py
+++ ecomm/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'nested_admin',
+
     'django_extensions',
 
     'rest_framework',
@@ -153,8 +155,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_ROOT = BASE_DIR / 'static'
 MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 
 # Default primary key field type
